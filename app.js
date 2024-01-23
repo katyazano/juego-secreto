b45f82464ba337b2f228fe93ca20d0eaa11b7628
katyazano <156813907+katyazano@users.noreply.github.com>
2024-01-22 22:30:07 -0600

    version 1 numero secreto

---
+++ app.js
@@ -0,0 +1,9 @@
+titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero secreto';
+
+parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un numero del 1 al 10';
+
+function intentoDeUsuario(){
+    alert('Clck desde el boton');
+}
